3e2fa25da1cc89ac3ba3c1891358eaa5072d17cd
RiyazulHaque64 <riyazulhaque64@gmail.com>
2024-01-07 02:41:23 +0600

    Connect to server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
-import app from "./app";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
-app.listen(9000, () => {
-  console.log(`pAccounts server is running on port ${9000}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(config.db_url);
+    app.listen(config.port, () => {
+      console.log(`pAccounts server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
